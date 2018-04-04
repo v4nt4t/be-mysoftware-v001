877591bdf0a9aed10e5912d0df09f567dbe8855b
irvan novianto <v4nt4t@gmail.com>
2018-04-04 10:27:45 +0700

    master files

---
+++ src/main/java/com/vsoft/mysoftware/MysoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.vsoft.mysoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class MysoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysoftwareApplication.class, args);
+	}
+}
